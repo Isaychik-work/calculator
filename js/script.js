687c853f1ffc379ef4bcae6a04721fc06d01f0ff
Антон Исаев <toni.getto.9@gmail.com>
2025-09-29 14:18:00 +0300

    Добавил ограничение по длине массива, реализовал кнопку стереть

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
 const numberButtons = document.querySelectorAll('.number_button'),
       calculator = document.querySelector('.calculator'),
       buttons = document.querySelectorAll('button'),
-      display = document.querySelector('.calculator__inner_form-display');
+      display = document.querySelector('.calculator__inner_form-display'),
+      removeButton = document.querySelector('.remove_button');
       
 // РЕАЛИЗАЦИЯ ВЫВОДА НА ДИСПЛЕЙ
 
@@ -26,43 +27,34 @@ function addCurrentNumber() {
          } else {
             display.innerHTML = currentNumber.join('');
          }
+         if (currentNumber.length > 20) {
+            currentNumber.pop();
+            display.innerHTML = currentNumber.join('');
+         }
       })
    })
 }
 
 addCurrentNumber();
 
-// buttons.forEach(btn => {
-//    btn.addEventListener('click', (e) => {
-//       e.preventDefault();
-//       target = e.target.innerHTML;
-//       currentNumber += target;
-//       display.innerHTML = currentNumber;
-//    })
-// })
+// РЕАЛИЗАЦИЯ КНОПКИ СТЕРЕТЬ
+
+function removeCurrentNumber() {
+   removeButton.addEventListener('click', (e) => {
+      currentNumber.length = 0;
+      display.innerHTML = currentNumber.join('');
+   })
+}
+
+removeCurrentNumber();
+
+// РЕАЛИЗАЦИЯ ФУНКЦИОНАЛЬНЫХ КНОПОК
 
 
 
 
 
 
-
-      // const calculatorCondition = {
-//     currentNumber: 0,
-//     previousNumber: null,
-//     operation: null,
-//     resetScreen: null
-// };
-
-// buttons.forEach(btn => {
-//     btn.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         const target = e.target.innerHTML;
-//         for (let i = 0; i<=9; i++) {
-//          calculatorCondition.currentNumber = `${target}`;
-//         }
-//     })
-// });
 
 
 
